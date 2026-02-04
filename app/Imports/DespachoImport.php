63c95703d404a2f36c9fd2f8fe6f18c5fd6e7a6e
juan <kall.su999@gmail.com>
2026-02-03 19:59:52 -0500

    Avance

---
+++ app/Imports/DespachoImport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Despacho;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+
+class DespachoImport implements ToModel, WithHeading
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Despacho([
+            //
+        ]);
+    }
+}
