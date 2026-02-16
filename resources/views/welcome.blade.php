114f327019d54c59a1e8b9f34fa9f987f7288b80
Your Name <youexample.com>
2026-02-16 18:08:22 -0500

    Avance proyecto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
 
+    <!-- 🔴 FAVICON -->
+    <link rel="icon" type="image/png" href="{{ asset('images/ico.png') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
